ed67d30e8fadd067e976d573f3d923cac5c5bd58
Azuremis <azuremismachine@gmail.com>
2021-03-16 12:52:55 +0000

    Add audio file input

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # setup basic app
 app = Flask(__name__)
 
  # route for homepage
-@app.route("/", methods=["GEt"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "hello world"
+    if request.method == "POST":
+        print("Form data received")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     # debug= rue for automatic refresh of app on save

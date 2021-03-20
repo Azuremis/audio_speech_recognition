98aab228688ec6502258d40cec1c4aa2363ddb2f
Azuremis <azuremismachine@gmail.com>
2021-03-20 23:08:54 +0000

    Transcribe audio input with Google speech recognition API

--- app.py
+++ app.py
@@ -1,16 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import speech_recognition as sr
 
 # setup basic app
 app = Flask(__name__)
 
- # route for homepage
+# route for homepage
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         print("Form data received")
+
+        # refresh the webpage if no valid file was submitted
+        if "File" not in request.files:
+            print("No valid file")
+            return redirect(request.url)
+
+        # handle empty transcribe request
+        file = request.files["File"]
+        if file.filename == "":
+            print("Empty file")
+            return redirect(request.url)
+
+        # assumption file exists
+        if file:
+            recogniser = sr.Recognizer()
+
+            # open audio in manageable format
+            audioFile = sr.AudioFile(file)
+
+            # read audio data
+            with audioFile as source:
+                data = recogniser.record(source)
+
+            # parse the text from audio
+            text = recogniser.recognize_google(data, key=None)
+            print(text)
+
     return render_template("index.html")
 
 if __name__ == "__main__":
-    # debug= rue for automatic refresh of app on save
+    # debug=True for automatic refresh of app on save
     # threaded=True for handling multiple requests
     app.run(debug=True, threaded=True)
